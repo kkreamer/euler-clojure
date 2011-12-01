08e2d46eb8de2bb43219fdc10cd839152be7b89e
Kevin Kreamer <kevin@kreamer.org>
2011-12-01 16:50:01 -0500

    Solve Problem 3

---
+++ problem-003.clj
@@ -0,0 +1,27 @@
+(defn sqrt-int [val]
+  (Math/round 
+   (Math/sqrt val)))
+
+(defn evenly-divisible [val divisor]
+  (= 0 (mod val divisor)))
+
+(defn maybe-prime-factors-descending [val]
+  (filter
+   #(evenly-divisible val %)
+   (range (sqrt-int val) 0 -1)))
+
+(defn prime? [val]
+  (= 1
+     (first
+      (maybe-prime-factors-descending val))))
+
+(defn largest-prime-factor [val]
+  (first
+   (filter prime?
+           (maybe-prime-factors-descending val))))
+
+(println
+ (largest-prime-factor 600851475143))
+
+;; (println
+;;  (largest-prime-factor 13195)) 
