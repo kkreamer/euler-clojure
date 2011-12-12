709c563308969ee5200f800fdb049b183b81d31a
Kevin Kreamer <kevin@kreamer.org>
2011-12-11 23:37:39 -0500

    Solved Problem 10

---
+++ problem-010.clj
@@ -0,0 +1,29 @@
+(defn sqrt-int [val]
+  (Math/round 
+   (Math/sqrt val)))
+
+(defn evenly-divisible [val divisor]
+  (= 0 (mod val divisor)))
+
+(defn maybe-prime-factors-descending [val]
+  (filter
+   #(evenly-divisible val %)
+   (range (sqrt-int val) 0 -1)))
+
+(defn prime? [val]
+  (= 1
+     (first
+      (maybe-prime-factors-descending val))))
+
+(defn primes []
+  (filter prime? (range 2 Double/POSITIVE_INFINITY)))
+
+(defn sum-of-primes-below [ceiling]
+ (reduce +
+         (take-while
+          #(< % ceiling)
+          (primes))))
+
+(println
+ (sum-of-primes-below 2000000))
+
