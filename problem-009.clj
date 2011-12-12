96bd971e0e4b7a9a9adde60ef7bb9adcf1d122be
Kevin Kreamer <kevin@kreamer.org>
2011-12-11 23:22:30 -0500

    Solved Problem 9

---
+++ problem-009.clj
@@ -0,0 +1,23 @@
+(defn pythagorean-triplet? [a b c]
+  (= (double c)     
+     (Math/sqrt
+      (+ (* a a)
+         (* b b)))))
+
+(defn answer? [a b c total]
+  (and (pythagorean-triplet? a b c)
+       (= total
+          (+ a b c))))
+(println
+ (for [c (range 1001)
+       b (range
+          (- 1001
+             c))
+       a (range
+          (- 1001
+             (+ b c)))
+       :when (and
+              (> c b)
+              (> b a)
+              (answer? a b c 1000))]
+   (* a b c)))
