2cd87af8fd20d8395c42c92d21d20dbe30c56424
Kevin Kreamer <kevin@kreamer.org>
2011-12-17 18:20:20 -0500

    Create new leiningen project

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject euler-clojure "1.0.0-SNAPSHOT"
+  :description "Project Euler <http://projecteuler.net> solutions implemented in clojure."
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dev-dependencies [[swank-clojure "1.3.3"]])
