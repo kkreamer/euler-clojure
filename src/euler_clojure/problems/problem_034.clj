8bbdaa1d8bc54c4260dbc01ee3d36490a976f008
Kevin Kreamer <kevin@kreamer.org>
2012-02-10 23:01:09 -0500

    Speed up problem 34 by precomputing the factorials

--- src/euler_clojure/problems/problem_034.clj
+++ src/euler_clojure/problems/problem_034.clj
@@ -3,15 +3,18 @@
         euler-clojure.util.factorial))
 
 (defn problem-034 []
-  (reduce +
-          (filter
-           (fn [n]
-             (= n
-                (reduce +
-                        (map factorial
-                             (digits n)))))
-           ;; This range starts at three since the problem says that 1 and 2
-           ;; not candidates, and ends where it does because 7 is where
-           ;; (n * 9!) < (9 * 10^n + 9 * 10^(n-1) + ...)
-           (range 3 (* 7 (factorial 9)))))) 
-
+  (let [factorials (vec
+                    (map
+                     (comp int factorial)
+                     (range 0 10)))]
+    (reduce +
+            (filter
+             (fn [n]
+               (= n
+                  (reduce +
+                          (map #(get factorials %)
+                               (digits n)))))
+             ;; This range starts at three since the problem says that 1 and 2
+             ;; not candidates, and ends where it does because 7 is where
+             ;; (n * 9!) < (9 * 10^n + 9 * 10^(n-1) + ...)
+             (range 3 2540160)))))
