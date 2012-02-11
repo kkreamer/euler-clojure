4ccbeca552b24228b010508eae50aff6c6f316e8
Kevin Kreamer <kevin@kreamer.org>
2012-02-10 20:51:01 -0500

    Solved problem 31

---
+++ src/euler_clojure/problems/problem_031.clj
@@ -0,0 +1,18 @@
+(ns euler-clojure.problems.problem-031)
+
+(defn coin-combos [^Integer total coins]
+  (let [this-coin (first coins)]
+    (if (= this-coin 1)
+      (list total)
+      (for [combo (range 0
+                         (inc (int (/ total this-coin))))
+            rest-combo (coin-combos (- total
+                                       (* combo this-coin))
+                                    (rest coins))]
+        (flatten (list combo rest-combo))))))
+
+(defn problem-031 []
+  (count
+   (coin-combos 200
+                '(200 100 50 20 10 5 2 1))))
+
