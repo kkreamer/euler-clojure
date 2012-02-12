ef8794985e8ae34c3a879187149ae4f444a689fd
Kevin Kreamer <kevin@kreamer.org>
2012-02-11 22:46:34 -0500

    Solve problem 37 using brute force and string conversions

---
+++ src/euler_clojure/problems/problem_037.clj
@@ -0,0 +1,34 @@
+(ns euler-clojure.problems.problem-037
+  (:use euler-clojure.util.digits
+        euler-clojure.util.primes))
+
+(defn truncate-with [f]
+  (fn [^Integer val]
+    (let [truncated-digits (f (digits val))]
+      (if (empty? truncated-digits)
+        0
+        (Integer. (apply str truncated-digits))))))
+
+(defn truncatable-with? [f ^Integer val]
+  ;; this is a bit confusing... if none of the values are not prime
+  ;; then that means that they all must be prime, hence truncatable.
+  (not-any? (complement prime?)
+            ;; reversing the array is an optimization: it's cheaper
+            ;; to determine if a single digit is prime versus a
+            ;; larger number.           
+            (reverse 
+             (take-while (complement zero?)
+                         (iterate (truncate-with f) val)))))
+    
+(defn truncatable? [^Integer val]
+  ;; according the problem description, 2, 3, 5, and 7 are not truncatable
+  (and (< 10 val) 
+       (truncatable-with? rest val)
+       (truncatable-with? butlast val)))
+
+(defn problem-037 []
+  (reduce +
+          (take 11 ;; problem description states there are 11 such primes
+                (filter truncatable? (primes)))))
+
+
