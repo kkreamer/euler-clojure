4b5e1d634522b48399e796eb18b2c1360bbcb3a0
Kevin Kreamer <kevin@kreamer.org>
2012-05-06 20:44:24 -0400

    Use require instead of use, so that we don't add specific problem-related function references into the environment

--- src/euler_clojure/core.clj
+++ src/euler_clojure/core.clj
@@ -1,7 +1,7 @@
 (ns euler-clojure.core
   (:use [clojopts.core :only [clojopts with-arg]]))
 
-(def PROBLEM-FN 'answer)
+(def problem-fn-name 'answer)
 
 (defn fatal-error
   ([] (System/exit -1))
@@ -18,28 +18,23 @@
     (catch IllegalArgumentException ex
       (fatal-error)))) ; clojopts prints its own error messages
 
-(defn problem-ns [^Integer number]
-  (symbol (str "euler-clojure.problems.problem-" (format "%03d" number))))
-
-(defn resolve-problem-fn [^Integer number]
-  (do
-    (try
-      (use (problem-ns number))
-      (catch java.io.FileNotFoundException ex
-        (fatal-error (str "ERROR: Could not find namespace for problem " number))))
-    (resolve PROBLEM-FN)))
-
 (defn problem-fn [^Integer number]
-  (let [fun (resolve-problem-fn number)]
-    (if (nil? fun)
-      (fatal-error (str "ERROR: Could not find answer for problem " number)))
-    fun))  
+  (try 
+    (let [ns-name (str "euler-clojure.problems.problem-" (format "%03d" number))
+          fn-name (symbol (str ns-name "/" problem-fn-name))
+          _ (require (symbol ns-name))
+          fun (resolve fn-name)]
+      (if (nil? fun)
+        (fatal-error (str "ERROR: Could not find answer for problem " number)))
+      fun)
+    (catch java.io.FileNotFoundException ex
+      (fatal-error (str "ERROR: Could not resolve answer for problem " number)))))      
 
 (defn run [{number :problem args :clojopts/more}]
   (apply (problem-fn number) args))
 
 (defmacro defproblem [& body]
-  `(defn ~PROBLEM-FN [& args#]
+  `(defn ~problem-fn-name [& args#]
      (time
       (println
        (apply (fn ~@body) args#)))))
