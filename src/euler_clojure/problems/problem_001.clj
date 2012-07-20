f0ff31a8f7ae438136340b762b44bdb25a991e2e
Kevin Kreamer <kevin@kreamer.org>
2012-07-19 23:06:45 -0400

    Generalize some of the code in problem 1.

--- src/euler_clojure/problems/problem_001.clj
+++ src/euler_clojure/problems/problem_001.clj
@@ -1,11 +1,21 @@
 (ns euler-clojure.problems.problem-001
-  (:use [euler-clojure.core :only [defproblem]]))
+  (:use [euler-clojure.core :only [defproblem]]
+        [euler-clojure.util.primes :only [evenly-divisible?]]))
+
+(defmacro evenly-divisible-by-any-of? [numerators]
+  `(fn [~'denom]
+     (or ~@(for [num# numerators]
+             `(evenly-divisible? ~'denom ~num#)))))
+
+(defn sum [coll]
+  (if (or (seq? coll)
+          (vector? coll))
+    (reduce + coll)
+    coll))
 
 (defproblem []
-  (reduce +
-          (filter
-           (fn [item]
-             (or (= (mod item 5) 0)
-                 (= (mod item 3) 0)))
-           (range 1000))))
+  (sum
+   (filter
+    (evenly-divisible-by-any-of? [3 5])
+    (range 1 1000))))
 
